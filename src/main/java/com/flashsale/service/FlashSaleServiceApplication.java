632a22ae3a40bcc2d65ab5e5c33d4fdb8914c386
dtnguyen96 <dung.christian.nguyen@gmail.com>
2026-04-23 19:15:07 +0700

    Spring initialization

---
+++ src/main/java/com/flashsale/service/FlashSaleServiceApplication.java
@@ -0,0 +1,13 @@
+package com.flashsale.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashSaleServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashSaleServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flashsale/service/FlashSaleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flashsale.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashSaleServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
